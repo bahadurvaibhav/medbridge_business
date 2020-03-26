538ab917c5ba9aef2e3d0705c402626e192e6ee1
bahadurvaibhav <bahadur.vaibhav@gmail.com>
2020-03-26 20:41:57 +0530

    medbridge app with 1. push notifications from firebase; 2. splash page

---
+++ lib/widget/HomePage.dart
@@ -0,0 +1,87 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:medbridge_business/domain/FcmNotification.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _homeScreenText = "Waiting for token...";
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  Widget _buildDialog(BuildContext context, FcmNotification item) {
+    return AlertDialog(
+      title: Text(item.title),
+      content: Text(item.body),
+      actions: <Widget>[
+        FlatButton(
+          child: const Text('CLOSE'),
+          onPressed: () {
+            Navigator.pop(context, false);
+          },
+        ),
+      ],
+    );
+  }
+
+  void _showItemDialog(Map<String, dynamic> message) {
+    showDialog<bool>(
+      context: context,
+      builder: (_) => _buildDialog(context, getNotification(message)),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        _showItemDialog(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+      const IosNotificationSettings(
+          sound: true, badge: true, alert: true, provisional: true),
+    );
+    _firebaseMessaging.onIosSettingsRegistered.listen(
+      (IosNotificationSettings settings) {
+        print("Settings registered: $settings");
+      },
+    );
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+        _homeScreenText = "Push Messaging token: $token";
+      });
+      print(_homeScreenText);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Push Messaging Demo'),
+      ),
+      body: Material(
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Text(_homeScreenText),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
