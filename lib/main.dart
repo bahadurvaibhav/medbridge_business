e854b9f49f88a3cc9187e63faa66bec38f8cd41b
bahadurvaibhav <bahadur.vaibhav@gmail.com>
2020-04-24 18:55:44 +0530

    added crashlytics and set ndk

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:medbridge_business/widget/SplashPage.dart';
 
 void main() {
+  Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
   runApp(MyApp());
 }
 
